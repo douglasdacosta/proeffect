e62eb2c4d9ed2e8c54a1b841c56e39cd0a4ac093
douglasdacosta <douglas@gmail.com>
2024-04-01 22:04:01 -0300

    Tela de manutenção de produção

--- config/adminlte.php
+++ config/adminlte.php
@@ -1,5 +1,23 @@
 <?php
 
+    use App\Http\Controllers\TelasController;
+
+    $telas = new TelasController();
+    // $categoriaTelas = $telas->getCategoriaTelas();
+
+    // $arra_menu[] = [
+    //                     'type'         => 'fullscreen-widget',
+    //                     'topnav_right' => true,
+    // ];
+
+    // foreach ($categoriaTelas as $key => $categoria) {
+    //     $arra_menu[] = ['header' => $categoria->nome];
+
+    //     $arra_menu[] ='';
+    // };
+
+
+
 return [
 
     /*
@@ -292,24 +310,13 @@ return [
     |
     */
 
+
     'menu' => [
-        // Navbar items:
-        // [
-        //     'type'         => 'navbar-search',
-        //     'text'         => 'search',
-        //     'topnav_right' => true,
-        // ],
         [
             'type'         => 'fullscreen-widget',
             'topnav_right' => true,
         ],
-
-        // Sidebar items:
-        // [
-        //     'type' => 'sidebar-menu-search',
-        //     'text' => 'search',
-        // ],
-        ['header' => 'Produção'],
+        ['header' => 'Cadastros'],
         [
             'text' => 'blog',
             'url'  => 'admin/blog',
@@ -371,13 +378,6 @@ return [
             'label'       => '',
             'label_color' => '',
         ],
-        // [
-        //     'text'        => 'Relatório produção',
-        //     'url'         => 'relatorio-producao',
-        //     'icon'        => 'far fa-fw fa-file',
-        //     'label'       => '',
-        //     'label_color' => '',
-        // ],
         [
             'text'        => 'Orçamentos',
             'url'         => 'orcamentos',
@@ -404,60 +404,6 @@ return [
             'url'  => 'admin/settings',
             'icon' => 'fas fa-fw fa-lock',
         ],
-        // [
-        //     'text'    => 'multilevel',
-        //     'icon'    => 'fas fa-fw fa-share',
-        //     'submenu' => [
-        //         [
-        //             'text' => 'level_one',
-        //             'url'  => '#',
-        //         ],
-        //         [
-        //             'text'    => 'level_one',
-        //             'url'     => '#',
-        //             'submenu' => [
-        //                 [
-        //                     'text' => 'level_two',
-        //                     'url'  => '#',
-        //                 ],
-        //                 [
-        //                     'text'    => 'level_two',
-        //                     'url'     => '#',
-        //                     'submenu' => [
-        //                         [
-        //                             'text' => 'level_three',
-        //                             'url'  => '#',
-        //                         ],
-        //                         [
-        //                             'text' => 'level_three',
-        //                             'url'  => '#',
-        //                         ],
-        //                     ],
-        //                 ],
-        //             ],
-        //         ],
-        //         [
-        //             'text' => 'level_one',
-        //             'url'  => '#',
-        //         ],
-        //     ],
-        // ],
-        // ['header' => 'labels'],
-        // [
-        //     'text'       => 'important',
-        //     'icon_color' => 'red',
-        //     'url'        => '#',
-        // ],
-        // [
-        //     'text'       => 'warning',
-        //     'icon_color' => 'yellow',
-        //     'url'        => '#',
-        // ],
-        // [
-        //     'text'       => 'information',
-        //     'icon_color' => 'cyan',
-        //     'url'        => '#',
-        // ],
     ],
 
     /*
