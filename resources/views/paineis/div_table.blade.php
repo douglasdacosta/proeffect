2fac5b196cf8e0761d8f90eb45e198d18ae2e0b9
douglasdacosta <douglas@gmail.com>
2024-08-31 16:14:52 -0300

    Ajuste de tela

--- resources/views/paineis/div_table.blade.php
+++ resources/views/paineis/div_table.blade.php
@@ -24,62 +24,25 @@
         </thead>
         <tbody>
             @foreach ($pedidosCompletos as $pedido)
-                @if(isset($pedido->colaboradores[$pedido->id]))
-                    <?php  $contador =0 ?>
-                    @foreach ($pedido->colaboradores[$pedido->id] as $colaborador)
-                        <tr>
-                            @if($contador==0)
-                                <td scope="col">{{$pedido->ep}}</td>
-                                <td scope="col">{{$pedido->os}}</td>
-                                <td scope="col">{{$pedido->qtde}}</td>
-                                <td scope="col">{{$pedido->qtde_blank}}</td>
-                                <td scope="col">{{$pedido->conjuntos}}</td>
-                                <td scope="col">{{ Carbon\Carbon::parse($pedido->data_entrega)->format('d/m/Y') }} </td>
-                                <td scope="col"class="{{ $pedido->class_dias_alerta }}">{{ $pedido->dias_alerta }}</td>
-                            @else
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            @endif
-                            <td scope="col">{{$colaborador['nome_etapa']}}</td>
-                            @if(isset($usinagem) && $usinagem == true)
-                                <td scope="col">{{$colaborador['numero_maquina']}}</td>
-                            @endif
-                            <td scope="col">{{$colaborador['select_motivo_pausas']}}</td>
-                            <td scope="col">{{$colaborador['texto_quantidade']}}</td>
-                            @if(isset($montagem) && $montagem == true)
-                                <td scope="col">{{$pedido->funcionario}}</td>
-                            @endif
-                            <td scope="col">{{$colaborador['nome']}}</td>
-                        </tr>
-                        <?php $contador++ ?>
-                    @endforeach
-                @else
                     <tr>
                         <td scope="col">{{$pedido->ep}}</td>
                         <td scope="col">{{$pedido->os}}</td>
                         <td scope="col">{{$pedido->qtde}}</td>
-                        <td scope="col">{{$pedido->qtde_blank}}</td>
+                        <td scope="col">{{$pedido->blanks}}</td>
                         <td scope="col">{{$pedido->conjuntos}}</td>
-                        <td scope="col">{{ Carbon\Carbon::parse($pedido->data_entrega)->format('d/m/Y') }} </td>
-                        <td scope="col"class="{{ $pedido->class_dias_alerta }}">{{ $pedido->dias_alerta }}</td>
-                        <td scope="col"></td>
-                        <td scope="col"></td>
-                        <td scope="col"></td>
+                        <td scope="col">@if($pedido->data_entrega !='') {{Carbon\Carbon::parse($pedido->data_entrega)->format('d/m/Y')}} @else {{''}} @endif</td>
+                        <td scope="col"class="{{ $pedido->class_dias_alerta }}">{{ $pedido->alerta }}</td>
+                        <td scope="col">Finalizado</td>
                         @if(isset($usinagem) && $usinagem == true)
-                            <td scope="col"></td>
+                        <td scope="col">{{$pedido->numero_maquina}}</td>
                         @endif
+                        <td scope="col"></td>
+                        <td scope="col"></td>
                         @if(isset($montagem) && $montagem == true)
                             <td scope="col"></td>
                         @endif
-                        <td scope="col"></td>
-                        <td scope="col"></td>
+                        <td scope="col">{{$pedido->colaborador}}</td>
                     </tr>
-                @endif
             @endforeach
         </tbody>
     </table>
@@ -110,62 +73,25 @@
         </thead>
         <tbody>
             @foreach ($pedidosPendentes as $pedido)
-                @if(isset($pedido->colaboradores[$pedido->id]))
-                    <?php $contador =0 ?>
-                    @foreach ($pedido->colaboradores[$pedido->id] as $colaborador)
-                        <tr>
-                            @if($contador==0)
-                                <td scope="col">{{$pedido->ep}}</td>
-                                <td scope="col">{{$pedido->os}}</td>
-                                <td scope="col">{{$pedido->qtde}}</td>
-                                <td scope="col">{{$pedido->qtde_blank}}</td>
-                                <td scope="col">{{$pedido->conjuntos}}</td>
-                                <td scope="col">{{ Carbon\Carbon::parse($pedido->data_entrega)->format('d/m/Y') }} </td>
-                                <td scope="col"class="{{ $pedido->class_dias_alerta }}">{{ $pedido->dias_alerta }}</td>
-                            @else
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            @endif
-                            <td scope="col">{{$colaborador['nome_etapa']}}</td>
-                            @if(isset($usinagem) && $usinagem == true)
-                                <td scope="col">{{$colaborador['numero_maquina']}}</td>
-                            @endif
-                            <td scope="col">{{$colaborador['select_motivo_pausas']}}</td>
-                            <td scope="col">{{$colaborador['texto_quantidade']}}</td>
-                            @if(isset($montagem) && $montagem == true)
-                                <td scope="col">{{$pedido->funcionario}}</td>
-                            @endif
-                            <td scope="col">{{$colaborador['nome']}}</td>
-                        </tr>
-                        <?php $contador++ ?>
-                    @endforeach
-                @else
                     <tr>
                         <td scope="col">{{$pedido->ep}}</td>
                         <td scope="col">{{$pedido->os}}</td>
                         <td scope="col">{{$pedido->qtde}}</td>
-                        <td scope="col">{{$pedido->qtde_blank}}</td>
+                        <td scope="col">{{$pedido->blanks}}</td>
                         <td scope="col">{{$pedido->conjuntos}}</td>
-                        <td scope="col">{{ Carbon\Carbon::parse($pedido->data_entrega)->format('d/m/Y') }} </td>
-                        <td scope="col"class="{{ $pedido->class_dias_alerta }}">{{ $pedido->dias_alerta }}</td>
+                        <td scope="col">@if($pedido->data_entrega !='') {{Carbon\Carbon::parse($pedido->data_entrega)->format('d/m/Y')}} @else {{''}} @endif</td>
+                        <td scope="col"class="{{ $pedido->class_dias_alerta }}">{{ $pedido->alerta }}</td>
+                        <td scope="col">{{$pedido->etapa}}</td>
                         @if(isset($usinagem) && $usinagem == true)
-                            <td scope="col"></td>
+                            <td scope="col">{{$pedido->numero_maquina}}</td>
                         @endif
-                        <td scope="col"></td>
-                        <td scope="col"></td>
-                        <td scope="col"></td>
-                        <td scope="col"></td>
+                        <td scope="col">{{$pedido->motivo_pausa}}</td>
+                        <td scope="col">{{$pedido->qtde_pausa}}</td>
                         @if(isset($montagem) && $montagem == true)
-                            <td scope="col"></td>
+                            <td scope="col">{{$pedido->responsavel}}</td>
                         @endif
-                        <td scope="col"></td>
+                        <td scope="col">{{$pedido->colaborador}}</td>
                     </tr>
-                @endif
             @endforeach
 
         </tbody>
