a49b379cfa0e120c05fb1c39ae33baa123ba25c6
douglasdacosta <you@example.com>
2025-01-17 13:53:12 -0300

    Relatório gerencial

--- resources/views/pedidos.blade.php
+++ resources/views/pedidos.blade.php
@@ -1312,7 +1312,6 @@ $palheta_cores = [1 => '#ff003d', 2 => '#ee7e4c', 3 => '#8f639f', 4 => '#94c5a5'
                                 <th scope="col"></th>
                                 <th scope="col"></th>
                                 <th scope="col"></th>
-                                <th scope="col"></th>
                                 <th scope="col">
                                     {{ PedidosController::formatarHoraMinuto($dado_pedido_status['totais']['total_tempo_usinagem']) . ' horas' }}
                                 </th>
@@ -1329,7 +1328,6 @@ $palheta_cores = [1 => '#ff003d', 2 => '#ee7e4c', 3 => '#8f639f', 4 => '#94c5a5'
                             </tr>
                             <tr>
                                 <th scope="col" colspan="2"></th>
-                                <th scope="col"></th>
                                 <th scope="col"></th>
                                 <th scope="col"></th>
                                 <th scope="col"></th>
