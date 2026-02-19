16d318f2d4c136e18cc9c670e9f4110a7053431d
douglasdacosta <doug.d.costa@gmail.com>
2026-02-18 22:10:59 -0300

    link prestador

--- public/js/renovacoes.js
+++ public/js/renovacoes.js
@@ -1,45 +1,47 @@
 $(document).ready(function() {
 
-    $('#table_renovacoes').DataTable({
-        "paging": true,
-        "lengthChange": true,
-        "searching": true,
-        "ordering": true,
-        "info": true,
-        "autoWidth": false,
-        "responsive": true,
-        "language": {
-            "sEmptyTable": "Nenhum registro encontrado",
-            "sInfo": "Mostrando _START_ até _END_ de _TOTAL_ registros",
-            "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
-            "sInfoFiltered": "(filtrado de _MAX_ registros no total)",
-            "sInfoPostFix": "",
-            "sInfoThousands": ".",
-            "sLengthMenu": "Mostrar _MENU_ registros por página",
-            "sLoadingRecords": "Carregando...",
-            "sProcessing": "Processando...",
-            "sSearch": "Pesquisar:",
-            "sUrl": "",
-            "sZeroRecords": "Nenhum registro encontrado",
-            "oPaginate": {
-                "sFirst": "Primeiro",
-                "sLast": "Último",
-                "sNext": "Próximo",
-                "sPrevious": "Anterior"
+    if ($('#table_renovacoes').length) {
+        $('#table_renovacoes').DataTable({
+            "paging": true,
+            "lengthChange": true,
+            "searching": true,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+            "language": {
+                "sEmptyTable": "Nenhum registro encontrado",
+                "sInfo": "Mostrando _START_ até _END_ de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando 0 até 0 de 0 registros",
+                "sInfoFiltered": "(filtrado de _MAX_ registros no total)",
+                "sInfoPostFix": "",
+                "sInfoThousands": ".",
+                "sLengthMenu": "Mostrar _MENU_ registros por página",
+                "sLoadingRecords": "Carregando...",
+                "sProcessing": "Processando...",
+                "sSearch": "Pesquisar:",
+                "sUrl": "",
+                "sZeroRecords": "Nenhum registro encontrado",
+                "oPaginate": {
+                    "sFirst": "Primeiro",
+                    "sLast": "Último",
+                    "sNext": "Próximo",
+                    "sPrevious": "Anterior"
+                },
+                "oAria": {
+                    "sSortAscending": ": ativar para ordenar coluna ascendente",
+                    "sSortDescending": ": ativar para ordenar coluna descendente"
+                }
             },
-            "oAria": {
-                "sSortAscending": ": ativar para ordenar coluna ascendente",
-                "sSortDescending": ": ativar para ordenar coluna descendente"
-            }
-        },
-        "columnDefs": [
-            {
-                "targets": [10, 11, 12],
-                "orderable": false,
-                "searchable": false
-            }
-        ]
-    });
+            "columnDefs": [
+                {
+                    "targets": [10, 11, 12],
+                    "orderable": false,
+                    "searchable": false
+                }
+            ]
+        });
+    }
 
     $(document).on('click', '.btn-finalizar-renovacao', function () {
         var id = $(this).data('id');
